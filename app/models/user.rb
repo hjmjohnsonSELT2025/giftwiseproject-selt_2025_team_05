a3965083d1dd2aff8b34944f86a18757a988f9ed
Kuan-Chieh <kuanchieh-huang@uiowa.edu>
2025-11-11 10:34:18 -0600

    added Preferences table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :event_users
   has_many :joined_events, through: :event_users, source: :event
+  has_many :preferences, dependent: :destroy
 end
