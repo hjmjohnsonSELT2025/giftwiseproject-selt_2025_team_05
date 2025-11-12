a186676f5c560674aa15c2ced3b1deff5d5f2fe0
Tatum Johnson <tatum-johnson@uiowa.edu>
2025-11-12 16:50:23 -0600

    remove_foreign_key manually when dropping users

--- db/migrate/20251111234435_drop_users.rb
+++ db/migrate/20251111234435_drop_users.rb
@@ -1,5 +1,8 @@
 class DropUsers < ActiveRecord::Migration[7.1]
   def change
+    remove_foreign_key :events, :users
+    remove_foreign_key :event_users, :users
+    remove_foreign_key :preferences, :users
     drop_table :users
   end
 end
