6cd4d7bf46912cdad508363d88692d942028a385
Trung Nguyen <trung-nguyen@uiowa.edu>
2025-11-12 12:57:12 -0600

    fix cannot view if not signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 #Home controller source: https://www.digitalocean.com/community/tutorials/how-to-set-up-user-authentication-with-devise-in-a-rails-7-application
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
     @upcoming_events = current_user.event_users
                                    .includes(:event)
