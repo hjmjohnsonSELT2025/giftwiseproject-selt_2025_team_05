82e35e7a8b49388b2259b7d28ed9bc6fa4b2dcd6
Tatum Johnson <tatum-johnson@uiowa.edu>
2025-11-11 19:54:56 -0600

    Add first_name, last_name, and birthdate to User sign up form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+#Home controller source: https://www.digitalocean.com/community/tutorials/how-to-set-up-user-authentication-with-devise-in-a-rails-7-application
 class HomeController < ApplicationController
   def index
     render
