4f70ddfe2d5cbc5bef76139da73a1c85b948357b
Trung Nguyen <trung-nguyen@uiowa.edu>
2025-11-12 12:39:26 -0600

    fix bug that only show event created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 #Home controller source: https://www.digitalocean.com/community/tutorials/how-to-set-up-user-authentication-with-devise-in-a-rails-7-application
 class HomeController < ApplicationController
   def index
-    @upcoming_events = current_user.events
-                                   .where(deleted: false)
-                                   .where("date >= ?", Date.today)
-                                   .order(:date)
+    @upcoming_events = current_user.event_users
+                                   .includes(:event)
+                                   .where(status: :joined)
+                                   .where("events.deleted = ? AND events.date >= ?", false, Date.today)
+                                   .references(:event)
+                                   .order("events.date")
   end
 end
