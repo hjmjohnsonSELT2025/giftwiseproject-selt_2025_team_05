fbce2e58f5a46f889cc1b73083ab2ef547bdb3a4
Tatum Johnson <tatum-johnson@uiowa.edu>
2025-11-13 16:33:51 -0600

    change enum syntax for ruby 8

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -2,12 +2,12 @@ class EventUser < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  enum status: {
+  enum :status, {
     invited: 0,
     joined: 1,
     declined: 2,
     left: 3
-  }, _default: :invited
+  }, default: :invited
 
   validates :status, presence: true
   validates :budget, numericality: { greater_than_or_equal_to: 0 }
