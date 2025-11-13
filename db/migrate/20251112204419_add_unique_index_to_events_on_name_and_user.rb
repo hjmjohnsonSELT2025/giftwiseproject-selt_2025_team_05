73ce93fe6c2eab6c03171767ac394a07ff6a82a6
Tatum Johnson <tatum-johnson@uiowa.edu>
2025-11-12 20:10:06 -0600

    Re-add foreign key user_id column to events, event_users, preferences:

--- db/migrate/20251112204419_add_unique_index_to_events_on_name_and_user.rb
+++ db/migrate/20251112204419_add_unique_index_to_events_on_name_and_user.rb
@@ -1,5 +1,8 @@
 class AddUniqueIndexToEventsOnNameAndUser < ActiveRecord::Migration[7.1]
   def change
+    add_reference :events, :user, foreign_key: true
+    add_reference :event_users, :user, foreign_key: true
+    add_reference :preferences, :user, foreign_key: true
     add_index :events, [:user_id, :name], unique: true
   end
 end
