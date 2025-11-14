545b9c6a9b8cc3fa6bf41e53ff7c9967205ce838
jpbleeden <jpbleeden@gmail.com>
2025-11-14 14:12:22 -0600

    Spec tests for RegistrationController almost all passing. The cancel method is tricky to cover, but i will try to make a workaround

--- spec/controllers/user_registrations_controller_spec.rb
+++ spec/controllers/user_registrations_controller_spec.rb
@@ -33,4 +33,64 @@ RSpec.describe Users::RegistrationsController, type: :controller do
       expect {post :create, params: {user: {first_name: "", last_name: "Last Name", birthdate: "2022-01-01", password: "password", email: "amy"}}}.not_to change(User, :count)
     end
   end
+
+  describe "should make the user wait to authenticate" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+
+    it "executes the inactive sign up redirect method" do
+      path = controller.send(:after_inactive_sign_up_path_for, user)
+      expect(path).to eq(controller.send(:after_sign_up_path_for, user))
+    end
+  end
+
+
+  describe "GET #new" do
+    before { @request.env["devise.mapping"] = Devise.mappings[:user] }
+    it "renders the new registration form" do
+      get :new
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+    it "assigns the current user" do
+      get :show
+      expect(assigns(:user)).to eq(user)
+      #expect(assigns(:resource)).to eq(user)       #Devise::RegistrationsController does not assign :resource on /GET #show
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET #edit" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+    it "renders the edit page" do
+      get :edit
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+    it "deletes the user" do
+      expect{delete :destroy}.to change(User, :count).by(-1)
+    end
+  end
+
+  #describe "GET #cancel" do    #I'm having trouble with getting this covered because there is no controller action that uses this
+  #end
+
+
 end
