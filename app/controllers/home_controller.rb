c086a1da503f4a6f30a2518ef25d6f0cd6329deb
Tatum Johnson <tatum-johnson@uiowa.edu>
2025-11-13 19:58:42 -0600

    add first name, last name, birthdate, bio to user table

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+#basic home controller setup from https://www.digitalocean.com/community/tutorials/how-to-set-up-user-authentication-with-devise-in-a-rails-7-application
+class HomeController < ApplicationController
+  def index
+    render
+  end
+end
