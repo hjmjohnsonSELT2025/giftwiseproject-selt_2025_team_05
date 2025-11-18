7ad6ff79ddea8b5aac401caa27d7e82816ae7cf5
jpbleeden <jpbleeden@gmail.com>
2025-11-18 13:10:39 -0600

    Added comment to AI generated line of code in User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_13_041825) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_14_173721) do
   create_table "event_users", force: :cascade do |t|
     t.integer "event_id", null: false
     t.integer "user_id", null: false
