f0e7c816c605ee399e634ce3fb4d46fe558a6432
Carlo Velarde <cav2003@live.com>
2025-11-25 15:04:29 -0600

    Set up service and controller for chatting with assistant

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :events do
     resources :event_users, only: [:create, :update, :show] do
       get :gift_suggestions, to: "gift_suggestions#show"
+      post :gift_suggestions, to: "gift_suggestions#create"
     end
   end
 
