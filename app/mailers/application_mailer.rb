9474d5c7fe03080daa984b976bcbe9a94b7e6a50
Kuan-Chieh <kuanchieh-huang@uiowa.edu>
2025-11-26 12:00:04 -0600

    Deploy to Heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: ENV['GMAIL_USERNAME']
   layout 'mailer'
 
+  # ============ DEVISE EMAILS ============
   # Tell Rails to look for templates in devise/mailer folder
   self.default template_path: 'devise/mailer'
 
@@ -11,5 +12,7 @@ class ApplicationMailer < ActionMailer::Base
     @reset_url = edit_user_password_url(reset_password_token: @token)
     mail(to: record.email, subject: 'Reset Your Password Instructions')
   end
+
+  # ============ CUSTOMIZED EMAIL FUNCTIONALITIES============
 
 end
