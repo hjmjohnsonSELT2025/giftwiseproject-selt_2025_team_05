4c0d3b8f78311af6881cc863e7ca62a7441dfc57
Tatum Johnson <tatum-johnson@uiowa.edu>
2025-11-27 11:24:21 -0600

    Edit add_item_to_wishlist feature to use the login step defined in auth_steps.rb

--- features/step_definitions/preference_steps.rb
+++ features/step_definitions/preference_steps.rb
@@ -1,11 +1,3 @@
-Given("I am a user logged in") do
-  @user = User.create!(email: "test@example.com", password: "password")
-  visit new_user_session_path
-  fill_in "Email", with: "test@example.com"
-  fill_in "Password", with: "password"
-  click_button "Sign in"
-end
-
 When("I visit the wishlist page") do
   visit preferences_path
 end
