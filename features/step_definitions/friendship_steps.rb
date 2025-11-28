1cf5228a94119a0e846e640f6c056db9e4a1074d
jpbleeden <jpbleeden@gmail.com>
2025-11-28 13:31:46 -0500

    Rewrote step def for searching friends table

--- features/step_definitions/friendship_steps.rb
+++ features/step_definitions/friendship_steps.rb
@@ -1,43 +1,68 @@
-Given('a user {string} exists') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('a user {string} exists') do |email|
+  User.find_or_create_by!(email: email) do |u|
+    u.password = "password"
+  end
 end
 
-Given('I am logged in as {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('I am logged in as {string}') do |email|
+  visit new_user_session_path
+  fill_in "Email", with: email
+  fill_in "Password", with: "password"
+  click_button "Sign in"
 end
 
 When('I go to the find friends page') do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit new_friendship_path
 end
 
 When('I press {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button string
 end
+
+#When('I fill in {string} with {string}') do |field, string|
+#fill_in field, with: string
+#end
 
 Then('I should not see {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).not_to have_content(string)
 end
 
-Given('{string} has sent a friend request to {string}') do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('{string} has sent a friend request to {string}') do |send, rec|
+  sender = User.find_by!(email: send)
+  receiver = User.find_by!(email: rec)
+  Friendship.create!(user: sender, friend: receiver, status: :pending)
 end
 
 When('I go to the friends page') do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit friendships_path
 end
 
 Then('I should see {string} on the friends page') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content(string)
 end
 
 Given('I am friends with {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  user   = User.find_by!(email: @current_user_email || User.first.email)
+  friend = User.find_by!(email: string)
+
+  Friendship.create!(
+    user: user,
+    friend: friend,
+    status: :accepted
+  )
 end
 
 Given('I have sent a friend request to {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  user = User.find_by!(email: @current_user_email || User.first.email)
+  friend = User.find_by!(email: string)
+  Friendship.create!(user: user, friend: friend, status: :pending)
 end
 
-Then('I should see {string} within the row for {string}') do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Then('I should see {string} within the row for {string}') do |text, email|
+  row = find(:xpath, "//tr[td[contains(., '#{email}')] and .//*[contains(., '#{text}')]]")
+  expect(row).to have_content(text)
+end
+
+Then('I sghould see {string} on the friends page') do |string|
+  expect(page).to have_content(string)
 end
