dab7d7d86fe56b4254548144c0c9babd80b842d3
jpbleeden <jpbleeden@gmail.com>
2025-11-28 12:40:39 -0500

    Wrote base step def structure

---
+++ features/step_definitions/friendship_steps.rb
@@ -0,0 +1,43 @@
+Given('a user {string} exists') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am logged in as {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I go to the find friends page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I press {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should not see {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('{string} has sent a friend request to {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I go to the friends page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see {string} on the friends page') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am friends with {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I have sent a friend request to {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see {string} within the row for {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
