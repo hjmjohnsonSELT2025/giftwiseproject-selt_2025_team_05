029a44ed4bc139b04ed9fc4ebcd9163bb41fe8ae
jpbleeden <jpbleeden@gmail.com>
2025-12-03 23:27:10 -0600

    Removed unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
     resources :event_users, only: [:create, :update, :show] do
       get :gift_suggestions, to: "gift_suggestions#show"
       post :gift_suggestions, to: "gift_suggestions#create"
-      get :edit_budget
       patch :update_budget
     end
   end
