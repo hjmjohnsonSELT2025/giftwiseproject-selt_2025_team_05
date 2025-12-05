8add6862b17932d7587402c7eca66164a518d601
Kuan-Chieh <kuanchieh-huang@uiowa.edu>
2025-12-05 15:32:45 -0600

    Modified the schedule for testing in production enviroment

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,15 @@
 # config/schedule.rb
 # Run daily at 9:00 AM
-every :day, at: '9:00 am' do
+# every :day, at: '9:00 am' do
+#   runner "SendEventRemindersJob.perform_later"
+# end
+
+# for testing(production):
+ every 2.minute do
   runner "SendEventRemindersJob.perform_later"
 end
 
-# for testing:
+# for testing(development):
 # env :PATH, ENV['PATH']
 # set :environment, "development"
 # set :output, { error: '/tmp/whenever_error.log', standard: '/tmp/whenever.log' }
