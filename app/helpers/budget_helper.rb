e6d1a695e79ab71c7d487618c569b5eeae9d037c
jpbleeden <jpbleeden@gmail.com>
2025-12-05 19:14:35 -0600

    Add helper method for determining if a user goes over budget by claiming a gift

--- app/helpers/budget_helper.rb
+++ app/helpers/budget_helper.rb
@@ -15,4 +15,10 @@ module BudgetHelper
     event_user.budget - purchased_total
   end
 
+  def can_claim_gift?(event, user, gift_cost)
+    event_user = event.event_users.find_by(user: user)
+    return true unless event_user&.budget.present?
+    claimed_total = total_claimed_for(event, user)
+    (claimed_total + gift_cost) <= event_user.budget
+  end
 end
