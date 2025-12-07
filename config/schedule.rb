d91ebd76886749f0afc92117f0f8a6fabded5baa
Kuan-Chieh <kuanchieh-huang@uiowa.edu>
2025-12-06 22:08:25 -0600

    Changed schedule file to make it clear that we only use cron on our local.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,12 @@
-# config/schedule.rb
-# Run daily at 9:00 AM
-# every :day, at: '9:00 am' do
-#   runner "SendEventRemindersJob.perform_later"
-# end
-
-# for testing(production):
- every 2.minute do
-  runner "SendEventRemindersJob.perform_later"
-end
-
-# for testing(development):
+# for testing:
 # env :PATH, ENV['PATH']
 # set :environment, "development"
 # set :output, { error: '/tmp/whenever_error.log', standard: '/tmp/whenever.log' }
 # every 1.minute do
 #   runner "SendEventRemindersJob.perform_now"
 # end
+#
+# Run daily at 9:00 AM
+every :day, at: '9:00 am' do
+  runner "SendEventRemindersJob.perform_now"
+end
