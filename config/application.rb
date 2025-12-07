d4bf441caaa0c42dfeeb0cc5f8cb70dd84d9302a
Kuan-Chieh <kuanchieh-huang@uiowa.edu>
2025-12-06 21:52:26 -0600

    Set Rails timezone to Central Time

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module GiftwiseprojectSelt2025Team05
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    # Set to Central Time to match Heroku
+    config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local
   end
 end
