af16dd24a0a50a9d4b410a7638d57265676f06da
Tatum Johnson <tatum-johnson@uiowa.edu>
2025-12-07 18:11:34 -0600

    change ConvertSuggestionsInPreferencesToSuggestionsObjects migration: should be if preference is not on user wishlist, then move it to the suggestions table

--- db/migrate/20251207231007_convert_suggestions_in_preferences_to_suggestions_objects.rb
+++ db/migrate/20251207231007_convert_suggestions_in_preferences_to_suggestions_objects.rb
@@ -1,7 +1,7 @@
 class ConvertSuggestionsInPreferencesToSuggestionsObjects < ActiveRecord::Migration[7.1]
   def change
     Preference.all.each do |preference|
-      if preference.on_user_wishlist
+      if !(preference.on_user_wishlist)
         #make new suggestion
         Suggestion.create!(user: preference.giver,
                            item_name: preference.item_name,
@@ -10,7 +10,7 @@ class ConvertSuggestionsInPreferencesToSuggestionsObjects < ActiveRecord::Migrat
                            purchased: preference.purchased,
                            event: preference.event,
                            recipient: preference.user)
-        preference.destroy
+        preference.destroy!
       end
     end
     remove_column 'preferences', 'on_user_wishlist'
