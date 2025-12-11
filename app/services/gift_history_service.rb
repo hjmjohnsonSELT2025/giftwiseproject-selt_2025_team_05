8474b8cad259bb95b60082718494335b225ec185
jpbleeden <jpbleeden@gmail.com>
2025-12-10 21:17:26 -0600

    Created gist history cervice to hold the logic for what gifts should appear on gift history page

---
+++ app/services/gift_history_service.rb
@@ -0,0 +1,58 @@
+class GiftHistoryService
+  def initialize(user, friend)
+    @user = user
+    @friend = friend
+  end
+
+  def fetch
+    shared_events = shared_events_between_users
+
+    upcoming = []
+    past = []
+
+    shared_events.each do |event|
+      prefs = relevant_preferences_for_event(event)
+
+      if upcoming_event?(event)
+        # include claimed OR purchased
+        upcoming += prefs
+      else
+        # for past events, only show purchased items
+        past += prefs.select(&:purchased)
+      end
+    end
+
+    upcoming.sort_by! { |p| p.event.date }
+    past.sort_by!    { |p| p.event.date }.reverse!
+
+    [upcoming, past]
+  end
+
+  def has_history?
+    upcoming, past = fetch
+    upcoming.any? || past.any?
+  end
+
+  private
+
+  def shared_events_between_users
+    Event.joins(:event_users)
+         .where(event_users: { user_id: @user.id })
+         .joins("INNER JOIN event_users eu2 ON eu2.event_id = events.id")
+         .where("eu2.user_id = ?", @friend.id)
+         .distinct
+  end
+
+  def relevant_preferences_for_event(event)
+    Preference.includes(:event)
+              .where(
+                event_id: event.id,
+                user_id:  @friend.id,
+                giver_id: @user.id
+              )
+  end
+
+  def upcoming_event?(event)
+    event.date >= Date.today
+  end
+end
